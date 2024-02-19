c390630698a657c52f72fbc518bed680582d0d97
Alfred <alfred@riverr.ai>
2024-02-18 23:30:36 -0600

    added astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://abboahene.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
