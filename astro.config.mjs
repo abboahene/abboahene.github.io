0bbfbbd31000e24cfb98668a103d27f11e382c34
Alfred <alfredboahene.7@gmail.com>
2024-04-03 14:45:12 -0500

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://abboahene.github.io",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), mdx()], 
 });
