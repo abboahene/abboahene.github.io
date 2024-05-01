c2eb1899c4cd13491217e11faa8e11f8233b9543
Alfred <alfredboahene.7@gmail.com>
2024-05-01 18:55:04 -0500

    fix toggle mobile sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,28 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
 import mdx from "@astrojs/mdx";
 import { transformerNotationDiff, transformerNotationHighlight } from "@shikijs/transformers";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://abboahene.github.io",
-  integrations: [UnoCSS({injectReset: true}), icon(), mdx()], 
+  integrations: [UnoCSS({
+    injectReset: true
+  }), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
       themes: {
         light: 'github-light',
-        dark: 'github-dark',
+        dark: 'github-dark'
       },
-      transformers: [transformerNotationDiff(), transformerNotationHighlight()],
+      transformers: [transformerNotationDiff(), transformerNotationHighlight()]
     },
     remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
+    rehypePlugins: [rehypeKatex]
   }
 });
